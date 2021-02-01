b9720b530eba3b26a5c0237c438551a4604a52d8
Hirushan272 <e17272@eng.pdn.ac.lk>
2021-02-01 16:49:42 +0530

    SaveTime And Date into the memory (#2)

--- lib/service/time_service.dart
+++ lib/service/time_service.dart
@@ -11,3 +11,13 @@ String formatTimeOfDay(TimeOfDay tod) {
   final format = DateFormat('HH:mm');
   return format.format(dt);
 }
+
+DateTime toDateTime(TimeOfDay tod) {
+  final now = new DateTime.now();
+  final dt = DateTime(now.year, now.month, now.day, tod.hour, tod.minute);
+  return dt;
+}
+
+String formateToTime(DateTime date) {
+  return DateFormat('HH:mm').format(date);
+}
